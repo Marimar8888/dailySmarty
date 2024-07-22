876669618e509707eee87e5e7642e566d3a76929
Marimar <malonso@quadram.mobi>
2024-07-22 12:24:34 +0200

    inserción del logo

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+import Logo from './logo';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DailySmarty</h1>
-
+        <div>
+          <Logo />
+        </div>
       </div>
     );
   }
