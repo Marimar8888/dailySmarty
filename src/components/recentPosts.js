0feab24018d73eb155fb4711bd21313c32acd1cd
Marimar <malonso@quadram.mobi>
2024-07-22 13:43:14 +0200

    setup redux form in searchbar.js

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 
-export default class RecentPosts extends Component {
+class RecentPosts extends Component {
     render() {
         return (
             <div className='recent-posts'>
                 <div className='recent-posts__wrapper'>
                     <div className='recent-posts__heading'>
-                        <ul className='recent-posts_posts'>
-                            <li>recent posts 0</li>
-                            <li>recent posts 1</li>
-                            <li>recent posts 2</li>
-                        </ul>
+                    <ul className='recent-posts_posts'>
+                        <li>recent posts 0</li>
+                        <li>recent posts 1</li>
+                        <li>recent posts 2</li>
+                    </ul>
                     </div>
                 </div>
             </div>
         )
     }
 }
+
+export default RecentPosts;
