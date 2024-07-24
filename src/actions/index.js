c3209e6479cb3f712f826d19f271ec683b8d9171
Marimar <malonso@quadram.mobi>
2024-07-24 14:13:42 +0200

    recentposts con error

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,4 @@
-/* import axios from 'axios';
-
-export function fetchRecentPosts() {
-  return function(dispatch) {
-    axios
-      .get('https://swapi.dev/api/people')
-      .then(response => {
-        console.log("response data fetchRecentPosts", response.data.results);
-      })
-  };
-} */
+import { SET_RECENT_POSTS } from './types';
 
 import axios from 'axios';
 
@@ -16,7 +6,11 @@ export function fetchRecentPosts() {
   return function(dispatch) {
     axios.get('https://swapi.dev/api/people')
       .then(response => {
-        console.log("response data fetchRecentPosts", response.data.results);
+        console.log("response data fetchRecentPosts", response.data);
+        dispatch({
+          type: SET_RECENT_POSTS,
+          payload: response.data
+        })
       })
       .catch(error => {
         console.error("Error fetching posts", error);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,9 @@
-/* import { combineReducers } from 'redux';
+import { combineReducers } from 'redux';
 
 import { reducer as form } from 'redux-form';
 
 const rootReducer = combineReducers({
-  form
-});
-
-export default rootReducer; */
-
-
-import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
-
-const rootReducer = combineReducers({
-  form: formReducer
+  form 
 });
 
 export default rootReducer;
