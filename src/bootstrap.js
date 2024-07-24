fc4071d797bb0c502bfed13259057fc2a2b1ac7e
Marimar <malonso@quadram.mobi>
2024-07-24 14:17:44 +0200

    recentposts update

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,31 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import thunk from 'redux-thunk';
-//import rootReducer from './reducers';
 
 import reducers from './reducers';
 
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+
 import Home from "./components/home";
 import Results from "./components/results";
-
+//import 'bootstrap/dist/css/bootstrap.css';
 import "./style/main.scss";
-//Da error con esta linea
-//const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-//const store = createStore(reducers);
-const store = createStore(
-  reducers,
-  applyMiddleware(thunk) 
-);
- 
 
 
 function main() {
   ReactDOM.render(
-    <Provider store={store}>
-   {/* <Provider store={createStoreWithMiddleware(reducers)}>  */}
+    <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={Home}/>
