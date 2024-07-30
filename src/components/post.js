a7edc3a1a516ea766793366c8f7b0d07c3256ca1
Marimar <malonso@quadram.mobi>
2024-07-30 12:19:13 +0200

    rederizando más datos

--- src/components/post.js
+++ src/components/post.js
@@ -1,14 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Post extends Component {
-  render() {
-    return (
-        <li>
-        {/* {post.title} */}
-        {this.props.name}
-    </li>
-    )
-  }
+
+   /*  renderTopics() {
+        let topics = this.props.associated_topics.map((topic, index) => {
+              return <span className="post-topic" key={index}>{topic}</span>
+        })
+        return topics;
+    } */
+
+         renderTopics() {
+        let films = this.props.films.map((film, index) => {
+              return <span className="post-topic" key={index}>{film}</span>
+        })
+        return films;
+    } 
+
+
+    render() {
+        return (
+            <li className="recent-post">
+                <div className="recent-post__title">
+                    {/* {this.props.title} */}
+                    {this.props.name}
+                </div>
+                <div className="recent-post__topics">
+                    {this.renderTopics()}
+                </div>
+
+            </li>
+        )
+    }
 }
 
 export default Post;
+
+
